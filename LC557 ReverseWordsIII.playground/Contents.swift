b2e5bc9dad0db9d9c6f6e179ad0a25bd7b6a853c
Gordon Choi <thefirate@gmail.com>
2021-09-30 00:15:03 +0900

    LeetCode No.557 (revisit)

--- LC557 ReverseWordsIII.playground/Contents.swift
+++ LC557 ReverseWordsIII.playground/Contents.swift
@@ -27,3 +27,35 @@ class Solution {
     }
 }
 
+
+class Solution {
+    func reverseWords(_ s: String) -> String {
+        var answer: String = ""
+        var spread = s.map { String($0) }
+        var left = 0
+        var right = 0
+        
+        while right < spread.count {
+            if spread[right] == " " {
+                var holder = spread[left..<right]
+                holder.reverse()
+                answer += holder.joined()
+                answer += " "
+                left = right+1
+                right += 1
+            } else if right == spread.count - 1 {
+                var holder = spread[left...right]
+                holder.reverse()
+                answer += holder.joined()
+                break
+            } else {
+                right += 1
+            }
+        }
+        
+        return answer
+        
+    }
+}
+
+// 2개의 포인터를 이용해 쪼오금 더 걸리는 시간이 줄어든 버전
