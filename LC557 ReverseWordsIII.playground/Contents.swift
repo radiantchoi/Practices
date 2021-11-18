27302555c88965cada319bc7b5ee52470415adec
Gordon Choi <thefirate@gmail.com>
2021-11-19 02:54:46 +0900

    LeetCode No.557 (revisit)

--- LC557 ReverseWordsIII.playground/Contents.swift
+++ LC557 ReverseWordsIII.playground/Contents.swift
@@ -59,3 +59,36 @@ class Solution {
 }
 
 // 2개의 포인터를 이용해 쪼오금 더 걸리는 시간이 줄어든 버전
+
+class Solution {
+    func reverseWords(_ s: String) -> String {
+        var answer = ""
+        let words = s.split(separator: " ")
+        
+        for word in words {
+            var container = word.map { String($0) }
+            var left = 0
+            var right = container.count-1
+            
+            while left < right {
+                var holder = container[left]
+                container[left] = container[right]
+                container[right] = holder
+                left += 1
+                right -= 1
+            }
+            
+            answer += container.joined()
+            answer += " "
+        }
+        
+        var answerArray = answer.map { String($0) }
+        answerArray.removeLast()
+        answer = answerArray.joined()
+        
+        return answer
+    }
+}
+
+// 스플릿을 사용해 단어 단위로 처리한 버전. 이게 출제 의도에 근접하지 않았을까?
+// 밑에 마지막 공백을 제거하는 방법은 더 좋은 방법이 있을 수도 있다.
