7530f19b6b9bec2861b3ac05d922ca5a136c5fa1
Gordon Choi <thefirate@gmail.com>
2022-01-08 19:26:16 +0900

    LeetCode No.867 Transpose Matrix

---
+++ LC867 TransposeMatrix.playground/Contents.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func transpose(_ matrix: [[Int]]) -> [[Int]] {
+        var result = Array(repeating: Array(repeating: 0, count: matrix.count), count: matrix[0].count)
+        
+        for i in 0..<matrix.count {
+            for j in 0..<matrix[i].count {
+                result[j][i] = matrix[i][j]
+            }
+        }
+        
+        return result
+    }
+}
+
+// 문제를 제대로 읽고, 쉽게 생각하자.
