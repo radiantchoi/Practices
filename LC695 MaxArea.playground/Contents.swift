1b97808b0afaf00ea6897bb6d2a5b0b220bf24cb
Gordon Choi <thefirate@gmail.com>
2022-02-23 00:25:29 +0900

    LeetCode No.695 (revisit)

--- LC695 MaxArea.playground/Contents.swift
+++ LC695 MaxArea.playground/Contents.swift
@@ -37,3 +37,39 @@ class Solution {
 // 떠올리지 못한 아이디어 : max를 통해 비교해야 할 count 변수를 어디에 넣을지
 // visited 2차원 배열을 따로 만들어서 해결하려고 했지만, grid를 복사한 후 체크한 셀은 0으로 만들어주는 방법도 있었다. 어차피 0 아니면 1이니까
 // 코드의 흐름에 대해 차분히 생각하기, 같은 자료를 여러 형식 및 관점에서 보는 방법 기르기, inout 문법(& 붙여야 함)에 대해 생각하기 등이 과제.
+
+
+class Solution {
+    func maxAreaOfIsland(_ grid: [[Int]]) -> Int {
+        var maxArea = 0
+        var visited = Array(repeating: Array(repeating: false, count: grid[0].count), count: grid.count)
+        
+        for y in 0..<grid.count {
+            for x in 0..<grid[y].count {
+                if grid[y][x] == 1 {
+                    var area = 0
+                    traverse(grid, &visited, x, y, &area)
+                    maxArea = max(area, maxArea)
+                }
+            }
+        }
+        
+        return maxArea
+    }
+    
+    func traverse(_ grid: [[Int]], _ visited: inout [[Bool]], _ x: Int, _ y: Int, _ area: inout Int) {
+        if x >= 0 && y >= 0 && x < grid[0].count && y < grid.count && grid[y][x] == 1 && visited[y][x] == false {
+            area += 1
+            visited[y][x] = true
+            traverse(grid, &visited, x+1, y, &area)
+            traverse(grid, &visited, x-1, y, &area)
+            traverse(grid, &visited, x, y-1, &area)
+            traverse(grid, &visited, x, y+1, &area)
+        } else {
+            return
+        }
+    }
+}
+
+// 확실히 성장했구나!!!
+// 이전에 참고한 풀이는 0과 1을 t/f처럼 활용했지만, 나는 별도의 t/f 그리드를 만들어서 넣었다. 아무래도 상관없지
