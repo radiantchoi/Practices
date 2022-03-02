c43d15e92f5ff40342b4fd6338912f418a7eaca9
Gordon Choi <thefirate@gmail.com>
2022-03-03 03:06:29 +0900

    LeetCode No.1281 (revisit)

--- LC1281 SubtractTheProductAndSumOfDigitsOfAnInteger.playground/Contents.swift
+++ LC1281 SubtractTheProductAndSumOfDigitsOfAnInteger.playground/Contents.swift
@@ -18,3 +18,13 @@ class Solution {
 
 // 문자열 다루기에 대해서 호되게 배우고 있는 듯 하다.
 // 다만 시간복잡도, 공간복잡도가 다 양호하게 떠서 그거에 대해서는 만족할 만 한 풀이
+
+class Solution {
+    func subtractProductAndSum(_ n: Int) -> Int {
+        let stringed = String(n)
+        let nums = stringed.map { Int(String($0))! }
+        return nums.reduce(1, *) - nums.reduce(0, +)
+    }
+}
+
+// 성장해서 돌아왔다!
